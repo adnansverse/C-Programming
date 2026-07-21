0f6008f174d78a15b11a582848b9020b39fd5356
Adnan Salim <adnansverse@gmail.com>
2026-07-21 19:34:41 +0600

    add data type 0.1

--- CHAPTER 01/03_data_types.c
+++ CHAPTER 01/03_data_types.c
@@ -8,6 +8,10 @@ int main() {
     char c;
     c = 'A';
 
+    printf("Integer value: %d\n", a); // Print integer value
+    printf("Float value: %.2f\n", b); // Print float value with 2 decimal places
+    printf("Character value: %c\n", c); // Print character value
+
 
     return 0; // Return 0 to indicate successful execution
 }
