b98d89143702a1fc7fc0990e307088a525ca16d5
Adnan Salim <adnansverse@gmail.com>
2026-07-26 23:37:47 +0600

    CHAPTER 02

---
+++ CHAPTER 02/problem-2.3.c
@@ -0,0 +1,14 @@
+// practise-2.3
+
+#include<stdio.h>
+int main(){
+int x,y;
+x = 1;
+y = x;
+x = 2;
+print("%d",y);
+
+
+
+return 0;
+}
---
+++ CHAPTER 02/problem_2.5.c
@@ -0,0 +1,10 @@
+// problem_2.4
+#include<stdio.h>
+int main(){
+int a = 50.55, b = 60.99, sum;  //type cast
+sum = a+b;
+printf(" %d + %d = %d", a,b, sum);
+
+
+return 0;
+}
