e73cfa9381ebb0ab630dd8e69f3af759e418ac87
Adnan Salim <adnansverse@gmail.com>
2026-09-06 00:54:06 +0600

    Adding some new chapter

---
+++ 15_Multidimensional_Arrays/01_2d_array_access.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
+    printf("%d\n", matrix[0][2]); // Access row 0, column 2
+    return 0;
+}
---
+++ 15_Multidimensional_Arrays/03_3d_array_declaration.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // 2 blocks, 4 rows, 3 columns
+    int example[2][4][3];
+    printf("3D Array declared successfully.\n");
+    return 0;
+}
